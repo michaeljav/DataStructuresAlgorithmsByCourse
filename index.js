8dad8c81e21f7c71a640559b41103e7d6aa5c447
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-05-30 21:17:17 -0400

    Exersice of Big O notations

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // const nemo =['nemo'];
 // const large = new Array(10000).fill('nemo');
 
@@ -15,11 +29,67 @@
 
 
 
-//O(1) Constant Time
-const nemo =[0,1,2];
-function findNemo(array) {
- console.log(array[0]); //O(1)
- console.log(array[1]); //O(1)
+// //O(1) Constant Time
+// const nemo =[0,1,2];
+// function findNemo(array) {
+//  console.log(array[0]); //O(1)
+//  console.log(array[1]); //O(1)
+// }
+
+// findNemo(nemo);//O(2) --> for convention O(1) Constant Time
+
+
+
+
+
+
+//1.EXERCISE Calculate Big O.  NOTE THIS WAY IN INTERVIEWS YOU WON'T USE IT
+function funChallenge(input) {
+  let a = 10; //O(1)
+  a = 50 + 3;//O(1)
+
+  for (let i = 0; i < input.length; i++) { //O(n) --> O(input) it's going to run n times
+    anotherFunction(); //O(n)
+    let stranger = true;//O(n)
+    a++; //O(n)
+  }
+  return a;//O(1)
 }
 
-findNemo(nemo);//O(2) --> for convention O(1) Constant Time
+//First
+//O(1) = 1 + 1 + 1
+//O(n) = n + n + n + n
+
+//Second
+//3
+//4n
+
+//Third
+//Big O(3 + 4n)
+
+
+//********THE WAY TO USE IN INTERVIEW IS SIMPLER*****
+//Big O(n)
+
+
+
+
+//2.EXERSICE
+function anotherFunChallenge(input) {
+  let a = 5;
+  let b = 10;
+  let c = 50;
+  for (let i = 0; i < input; i++) { //O(n)
+    let x = i + 1;
+    let y = i + 2;
+    let z = i + 3;
+
+  }
+  for (let j = 0; j < input; j++) { //O(n)
+    let p = j * 2;
+    let q = j * 2;
+  }
+  let whoAmI = "I don't know";
+}
+//O(4 + 5n)
+//O(n)
