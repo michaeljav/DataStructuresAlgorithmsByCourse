4a83107a6e01121f7d140f5fa6846ddc308c20e9
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-06-09 19:11:24 -0400

    Inserting Node which uses reference

--- index.js
+++ index.js
@@ -16,7 +16,7 @@
 // }
 
 class Node {
-  cunstructor(value) {
+  constructor(value) {
     this.value = value;
     this.next  = null;
   }
@@ -40,7 +40,7 @@ class LinkedList {
     return this;
   }
 
-  prepend(value) {
+  prepend(newValue) {
     const newNode =  new Node(newValue);
     newNode.next = this.head;
     this.head = newNode;
@@ -48,13 +48,95 @@ class LinkedList {
     return this;
   }
 
+  printList(){
+    const array = [];
+    let currentNode = this.head;
+
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
+  insert(index, value){
+    //check params
+    
+    if (index >= this.length) {
+      return;
+    }
+  //  debugger;
+
+    const newNode = new Node(value);
+    
+
+    /**ESTO  ES LA EXPLICACION DE COMO FUNCIONA LA INSERCION */
+    /*
+    //******Insertaré en el segundo nodo el nuevo nodo****
+    //aqui estan todos los nodos 10-->5-->16-->20 || insertar segundo nodo 999
+
+    //Comienzo haciendo referencia a header que tiene todos 
+    //los node
+    let refecenHead = this.head;
+    //Almaceno  desde el segundo nodo hacia adelante.
+    //NOTA: AL YO  HACER (refecenHead.next) SIMPLEMENTE ESTOY ATRVESANDO
+    //AL SIGUIENTE NODO PERO EN MEMORIO  ESTA EL O LOS NODOS ANTERIORES.    
+    //5-->16-->20-->null
+    let SecondNodeForward = refecenHead.next;
+    //Al heder  en su segundo nodo (next) se le inserta el nuevo nodo
+    //10-->5-->16-->20 : 
+    //resultado 10-->999-->null
+    refecenHead.next =newNode;
+    //En el Nodo nuevo inserto en su next  los nodos desde el segundo hacia delante 
+    //10-->999-->null :
+    //Result: 10-->999-->5-->16-->20-->null 
+    newNode.next = SecondNodeForward;
+    */
+ 
+    
+    //Reference to the head
+    //10-->5__-->16-->20
+    const leader = this.traverseToIndex(index-1); 
+    // 16-->20
+    const holdingPointer = leader.next;
+    // //10-->5-->99-->null
+    leader.next = newNode;
+     //10-->5-->99-->16-->20
+    newNode.next = holdingPointer;
+
+
+    this.length++;
+    return this.printList();    
+
+  }
+
+  traverseToIndex(index) {
+    //check params
+    let counter = 0;
+    
+    let currentNode = this.head;
+    while (counter !== index) {
+      currentNode = currentNode.next;
+      counter++;
+    }
+
+    return currentNode;
+  }
+
+
 }
 
-const myLinkedList = new LinkedList(1);
+const myLinkedList = new LinkedList(10);
 
 // console.log(myLinkedList);
-myLinkedList.append(2);
-myLinkedList.append(3);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.append(20);
+// myLinkedList.prepend(1);
+myLinkedList.insert(2,999);
+
+
+console.log(myLinkedList.printList());
 
 
 // console.log(myLinkedList);
